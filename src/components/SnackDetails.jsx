10ef3c1d7c898395e77b1c09760cecd2a4b00a33
JayZ44 <jazonyounge@pursuit.org>
2024-07-22 11:21:02 -0400

    delete route sort of working

--- src/components/SnackDetails.jsx
+++ src/components/SnackDetails.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {  useParams } from "react-router-dom";
-import '../styles/snackDetails.css'
-
+import { useParams } from "react-router-dom";
+import "../styles/snackDetails.css";
 
 const API = import.meta.env.VITE_API_URL;
 const SnackDetails = () => {
   const [snacks, setSnacks] = useState([]);
   let { index } = useParams();
+
+  const deleteSnack = () => {
+    fetch(`${API}/snacks/${snacks.id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json)
+      .then((res) => {
+        nav("/snacks");
+      })
+      .catch((err) => console.log(err));
+  };
 
   useEffect(() => {
     fetch(`${API}/snacks/${index}`)
@@ -20,13 +30,37 @@ const SnackDetails = () => {
   }, []);
   return (
     <div className="snack-details">
-      <h2 ><strong className="label">{snacks.name}</strong></h2>
-      <p><strong className="label">Origin: </strong>{snacks.description}</p>
-      <p><strong className="label">Description: </strong>{snacks.description}</p>
-      <p><strong className="label">Rating: </strong>{snacks.rating}</p>
-      <p><strong className="label">Vegetarian: </strong>{snacks.is_vegetarian ? "Yes" : "No"}</p>
-      <p><strong className="label">Discovered Date: </strong>{snacks.discovered_date}</p>
-      <p><strong className="label">Comments: </strong>{snacks.comments}</p>
+      <h2>
+        <strong className="label">{snacks.name}</strong>
+      </h2>
+      <p>
+        <strong className="label">Origin: </strong>
+        {snacks.description}
+      </p>
+      <p>
+        <strong className="label">Description: </strong>
+        {snacks.description}
+      </p>
+      <p>
+        <strong className="label">Rating: </strong>
+        {snacks.rating}
+      </p>
+      <p>
+        <strong className="label">Vegetarian: </strong>
+        {snacks.is_vegetarian ? "Yes" : "No"}
+      </p>
+      <p>
+        <strong className="label">Discovered Date: </strong>
+        {snacks.discovered_date}
+      </p>
+      <span>
+        <button onClick={deleteSnack}>Delete Snack</button>
+        <button>Edit Snack</button>
+      </span>
+      <p>
+        <strong className="label">Comments: </strong>
+        {snacks.comments}
+      </p>
     </div>
   );
 };
