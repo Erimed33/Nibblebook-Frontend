0af25619e6d0e292da456353145325b0fc41ef7d
Erimed33 <erikamedina@pursuit.org>
2024-07-23 08:18:03 -0400

    homepage images complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../styles/home.css'
+import cazu from "../images/cazu.png"
+import centuryegg from  "../images/centuryegg.png"
+import friedSpiders from "../images/friedSpiders.png"
+import '../styles/home.css'
 
 const Home = () => {
     return (
-        <div >
+        <div className="home-container">
            <h1>Welcome to the Home Page</h1> 
+           <img src={cazu} alt="Welcome" className="gallery-image" />
+           <img src={centuryegg} alt="Welcome" className="gallery-image" />
+           <img src={friedSpiders} alt="Welcome" className="gallery-image" />
         </div>
-    );
+    )
 };
 
 export default Home;
